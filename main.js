f853b89880302730bb31397c23917aa33ce72d33
Nmich <nmich.cor@gmail.com>
2019-11-05 15:30:46 +0100

    correction boucle forEach - modification event click - refactorisation code

--- main.js
+++ main.js
@@ -1,46 +1,72 @@
 'use strict'
 
-const datas = './statements.json';
-//créer une div pour les énoncés
-const currentDiv = document.getElementById('current-div');
-let newArticle = document.createElement('article');
-let newDiv = document.createElement('div');
-newDiv.className = 'new-div';
+const dataUrl = './statements.json'
+
+//cibler l'élément que l'on va modifier
+const statementContainer = document.querySelector('.hero-body .container')
+
+//récupération des données
+function fetchJSON(url) {
+    return fetch(url)
+        .then(function (resp) {
+            return resp.json()
+        })
+        .catch(function (err) {
+            throw err
+        })
+}
+
+// fonction qui prend comme argumnent le nom, la classe et le text
+// pour créer des éléments html
+function createTag(tagName, className, text,image) {
+    const tag = document.createElement(tagName)
+    tag.className = className
+    tag.textContent = text
+    tag.image = image
+    return tag
+}
+
+function createTitle(title) {
+    return createTag('h2', 'new-title title', title)
+}
+
+function createSubTitle(subtitle) {
+    return createTag('h3', 'new-subtitle subtitle', subtitle)
+}
+
+function createText(content) {
+    return createTag('p', '', content)
+}
 
 function createArticle(contentArticle) {
 
-    const {title, subtitle, content} = contentArticle 
-    const newTitle = document.createElement('h2');
-    newTitle.className = 'new-title title';
-    const newSubtitle = document.createElement('p')
-    newSubtitle.className = 'new-subtitle subtitle';
-    const newText = document.createElement('p');
-    newText.className = '';
-    newTitle.textContent = title;
-    newSubtitle.textContent = subtitle;
-    newText.textContent = content;
-    newArticle.append(newTitle, newSubtitle, newText);
+    const {
+        title,
+        subtitle,
+        content
+    } = contentArticle
 
-    return newArticle;
+    const titleTag = createTitle(title)
+    const subtitleTag = createSubTitle(subtitle)
+    const contentTag = createText(content)
+
+    const articleTag = document.createElement('article')
+    articleTag.className = 'box'
+    articleTag.append(titleTag, subtitleTag, contentTag)
+
+    return articleTag
 }
+
 
 function createDiv(statements) {
-    statements.forEach(function (statement) {
-        const article = createArticle(statement);
-        newDiv.append(article);
-    });
-    return newDiv;
-}
+    const newDiv = document.createElement('div')
+    newDiv.className = 'new-div'
 
-//récupération des datas
-function fetchJSON(u) {
-    return fetch(u)
-        .then(function (resp) {
-            return resp.json();
-        })
-        .catch(function (err) {
-            throw err;
-        });
+    statements.forEach(function (statement) {
+        const article = createArticle(statement)
+        newDiv.append(article)
+    })
+    return newDiv
 }
 
 
@@ -54,68 +80,39 @@ const items = [
 ]
 
 document.addEventListener("DOMContentLoaded", () => {
-    //récupération des datas
-    fetchJSON(datas)
+    //récupération des données
+    fetchJSON(dataUrl)
         .then(function (statements) {
-            
+
+            const navbarEnd = document.getElementById('navbar-end')
+
+            //ecouter les évènemzents au click
+            navbarEnd.addEventListener('click', event => {
+                // variable qui prend comme argument le début et la fin d'un tableau
+                // .split('-') recupère le texte dans l'item qui est clické
+                const [start, end] = event.target.textContent.split('-')
+                //créer un nouveau tableau virtuel avec slice()
+                const part = statements.slice(+start - 1, +end)
+                const newDiv = createDiv(part)
+
+                statementContainer.firstElementChild.replaceWith(newDiv)
+            })
+
             //pour chaque items,
-            const navbarEnd = document.getElementById('navbar-end');
             items.forEach(item => {
                 //creer un <a>
-                const navbarItem = document.createElement('a');
+                const navbarItem = document.createElement('a')
                 //ajouter la class
-                navbarItem.className = 'navbar-item num';
+                navbarItem.className = 'navbar-item num'
                 //mettre le texte dans le <a> 
-                navbarItem.textContent = item;
+                navbarItem.textContent = item
                 //ajoute les <a> en fin de balise
-                navbarEnd.appendChild(navbarItem);
-                // console.log(navbarItem)
-                //ecouter l'evenement au click des a.nav-item.num
-                
-                //declarer une variable qui est = à newDiv 
-                //pour eviter la création d'un nouveau
-                navbarItem.addEventListener('click', () => {
-                    const partOne = statements.slice(0, 25)
-                    const partTwo = statements.slice(25, 50)
-                    const partThree = statements.slice(50, 75)
-                    const partFour = statements.slice(75, 100)
-                    const partFive = statements.slice(100, 125)
-                    const partSix = statements.slice(125, 150)
-                    switch (item) {
-                            case '1-25':
-                               createDiv(partOne);
-                                break;
-                            case '26-50':
-                                createDiv(partTwo);
-                                break;
-                            case '51-75':
-                                createDiv(partThree);
-                                break;
-                            case '76-100':
-                            createDiv(partFour);
-                            break;
-                        case '101-125':
-                            createDiv(partFive);
-                            break;
-                        case '126-150':
-                            createDiv(partSix);
-                            break;
-                        default:
-                            createDiv();
-                            break;
-                    }
-                    
-                    // newArticle = nArticle;
-                    
-                    // newDiv.append(nArticle)
-                    
-                    currentDiv.replaceWith(newDiv);
-                });
-            });
+                navbarEnd.appendChild(navbarItem)
+            })
 
         })
         .catch(function (e) {
-            console.error(e);
-        });
+            console.error(e)
+        })
 
-});
+})
